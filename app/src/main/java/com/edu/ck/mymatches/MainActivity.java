a58b545655ba11754bb9cc5b147ca1ee6ef9189c
sasou10 <sara.sandid@edu.ece.fr>
2020-03-14 14:29:53 +0100

    version1

--- app/src/main/java/com/edu/ck/mymatches/MainActivity.java
+++ app/src/main/java/com/edu/ck/mymatches/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
