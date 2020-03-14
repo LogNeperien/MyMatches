2bb8c03c97083eb8536ac8b4574a1d7b6ef9a6e5
unknown <helene.carlier.gubler@gmail.com>
2020-03-14 16:01:18 +0100

    page fini without Database

--- app/src/main/java/com/edu/ck/mymatches/Match.java
+++ app/src/main/java/com/edu/ck/mymatches/Match.java
@@ -2,6 +2,7 @@ package com.edu.ck.mymatches;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Match extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class Match extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+        Intent intent = getIntent();
+
+
     }
 }
