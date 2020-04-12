ba367e9fcf421f45d71225abfc963bd47a48d364
unknown <helene.carlier.gubler@gmail.com>
2020-04-12 12:11:09 +0200

    ajout bibliothèque graphView

--- app/src/main/java/com/edu/ck/mymatches/Statistiques.java
+++ app/src/main/java/com/edu/ck/mymatches/Statistiques.java
@@ -13,6 +13,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Statistiques extends AppCompatActivity {
 
@@ -26,7 +32,7 @@ public class Statistiques extends AppCompatActivity {
     private int e2_60 = -1;
     private int e2_80 = -1;
 
-    private TextView TextView2, TextView5, TextView6, TextView7;
+    private TextView TextView2, TextView5, TextView6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +55,30 @@ public class Statistiques extends AppCompatActivity {
         TextView2 = findViewById(R.id.textView2);
         TextView5 = findViewById(R.id.textView5);
         TextView6 = findViewById(R.id.textView6);
-        TextView7 = findViewById(R.id.textView7);
 
         TextView2.setText(Integer.toString(e1_20));
         TextView5.setText(Integer.toString(e1_40));
         TextView6.setText(Integer.toString(e2_20));
-        TextView7.setText(Integer.toString(e2_40));
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        LineGraphSeries<DataPoint> serieEquipe1 = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 0),
+                new DataPoint(20, e1_20),
+                new DataPoint(40, e1_40),
+                new DataPoint(60, e1_60),
+                new DataPoint(80, e1_80)
+        });
+
+        LineGraphSeries<DataPoint> serieEquipe2 = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 0),
+                new DataPoint(20, e2_20),
+                new DataPoint(40, e2_40),
+                new DataPoint(60, e2_60),
+                new DataPoint(80, e2_80)
+        });
+
+        graph.addSeries(serieEquipe1);
 
     }
 
